616e4cb364bfe9b037083a3d507fa1f0c03a7fa5
Kerwin Huang <qwerzxcv98@gmail.com>
2026-07-11 21:30:05 +0800

    feat: implement Model wrapper in src/model.rs

--- src/model.rs
+++ src/model.rs
@@ -1,1 +1,18 @@
-pub use model_shared::Model;
+use burn::module::Module;
+use burn::prelude::*;
+use model_shared::UNet;
+
+#[derive(Module, Debug)]
+pub struct Model<B: Backend> {
+    pub unet: UNet<B>,
+}
+
+impl<B: Backend> Model<B> {
+    pub fn new(device: &B::Device, num_classes: usize) -> Self {
+        // We set base_dim to 32 channels. This provides a great balance of
+        // generation quality and lightweight CPU inference speed.
+        Self {
+            unet: UNet::new(device, num_classes, 32),
+        }
+    }
+}
